b9027f75e98dd2f888f1ce8b035fd79a520e4f69
Vishal Khanvilkar <vishalkhanvilkar00012@gmail.com>
2025-02-21 12:02:13 +0530

    Added User model

--- models/User.js
+++ models/User.js
@@ -1,10 +1,42 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  auth0Id: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  avatar: { type: String },
-});
+const userSchema = new mongoose.Schema(
+  {
+    auth0Id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    avatar: {
+      type: String,
+      default: "",
+    },
+    budget: {
+      type: Number,
+      default: 0,
+    },
+    currency: {
+      type: String,
+      default: "USD",
+    },
+    transactions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Transaction",
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("User", UserSchema);
+const User = mongoose.model("User", userSchema);
+export default User;
